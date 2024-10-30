dad9c09184ff51eea3b5873be3414e0a499f213b
Gokul Nair <gokul.getpowerplay.in>
2024-10-30 12:34:34 +0530

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MediaBrowser",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MediaBrowser",
+            targets: ["MediaBrowser"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MediaBrowser"),
+        .testTarget(
+            name: "MediaBrowserTests",
+            dependencies: ["MediaBrowser"]
+        ),
+    ]
+)
